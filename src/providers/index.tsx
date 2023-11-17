6a081783817f4bd0a52be5db9e906d1abc0981b7
gfmanica <gabrielfelipemanica@gmail.com>
2023-11-16 23:45:34 -0300

    inicado projeto

---
+++ src/providers/index.tsx
@@ -0,0 +1,10 @@
+import { NextUIProvider } from '@nextui-org/react';
+import { ReactNode } from 'react';
+
+type TProviders = {
+  children: ReactNode;
+};
+
+export default function Providers({ children }: TProviders) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
