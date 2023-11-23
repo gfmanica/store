5b35ffc7be57b8b33e8671c51aa16d628491d30f
Gabriel Felipe Manica <gabriel.manica@ciss.com.br>
2023-11-23 15:51:38 -0300

    teste de conexao com banco

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -9,8 +9,14 @@ export async function GET(request: NextRequest) {
   const prisma = new PrismaClient({
     datasourceUrl: `postgres://${user}:${password}@ep-twilight-scene-54661059-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15`,
   });
+  let a = '';
 
-  // const a = await prisma.$connect();
+  try {
+    await prisma.$connect();
+    a = 'deu certo';
+  } catch (error) {
+    a = 'falhouse';
+  }
 
-  return NextResponse.json('teste conexao');
+  return NextResponse.json(a);
 }
