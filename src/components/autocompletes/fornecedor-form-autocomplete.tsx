29cb6e07820c68bd78149976d4181b8910bee42c
gfmanica <gabrielfelipemanica@gmail.com>
2023-11-26 17:56:46 -0300

    feito carregar o autocomplete quandoe entra pra editar

--- src/components/autocompletes/fornecedor-form-autocomplete.tsx
+++ src/components/autocompletes/fornecedor-form-autocomplete.tsx
@@ -4,7 +4,7 @@ import { TFornecedor } from '@/types';
 import { Autocomplete, AutocompleteItem } from '@nextui-org/react';
 import { useQuery } from '@tanstack/react-query';
 import { cn } from '@/utils/cn';
-import { HTMLProps, useState } from 'react';
+import { HTMLProps, useEffect, useState } from 'react';
 import {
   Control,
   Controller,
@@ -35,6 +35,7 @@ export default function FornecedorFormAutocomplete<
   const { isAuthenticated } = useAuthContext();
   const Api = useApiContext();
   const [selectedKey, setSelectedKey] = useState<string>('');
+  const [fornecedores, setFornecedores] = useState<TFornecedor[]>([]);
 
   const { data, isFetching } = useQuery<TFornecedor[]>({
     queryKey: ['getFornecedores'],
@@ -47,37 +48,43 @@ export default function FornecedorFormAutocomplete<
     <Controller
       name={name}
       control={control}
-      render={({ field: { onChange, value } }) => (
-        <Autocomplete
-          defaultItems={data || [value]}
-          label={label}
-          disabled={disabled}
-          isLoading={isFetching}
-          className={cn(className)}
-          isInvalid={!!error}
-          errorMessage={error ? error?.message : ''}
-          selectedKey={selectedKey}
-          onSelectionChange={(key: string) => {
-            const fornecedor = data?.find(
-              (item) => item.idFornecedor === Number(key)
-            );
+      render={({ field: { onChange, value } }) => {
+        if (data && !selectedKey && value) {
+          setSelectedKey(value.idFornecedor.toString());
+        }
+        
+        return (
+          <Autocomplete
+            defaultItems={data || []}
+            label={label}
+            disabled={disabled}
+            isLoading={isFetching}
+            className={cn(className)}
+            isInvalid={!!error}
+            errorMessage={error ? error?.message : ''}
+            selectedKey={selectedKey}
+            onSelectionChange={(key: string) => {
+              const fornecedor = data?.find(
+                (item) => item.idFornecedor === Number(key)
+              );
 
-            if (fornecedor) {
-              onChange(fornecedor);
-              setSelectedKey(key);
-            } else {
-              onChange(null);
-              setSelectedKey('');
-            }
-          }}
-        >
-          {(fornecedor: TFornecedor) => (
-            <AutocompleteItem key={fornecedor.idFornecedor}>
-              {fornecedor.dsFornecedor}
-            </AutocompleteItem>
-          )}
-        </Autocomplete>
-      )}
+              if (fornecedor) {
+                onChange(fornecedor);
+                setSelectedKey(key);
+              } else {
+                onChange(null);
+                setSelectedKey('');
+              }
+            }}
+          >
+            {(fornecedor: TFornecedor) => (
+              <AutocompleteItem key={fornecedor.idFornecedor}>
+                {fornecedor.dsFornecedor}
+              </AutocompleteItem>
+            )}
+          </Autocomplete>
+        );
+      }}
     />
   );
 }
